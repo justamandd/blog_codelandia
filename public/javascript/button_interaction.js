b15cceb1effa392f666d603d7efb6473727430cc
justamandd <justamandd@gmail.com>
2022-06-18 04:26:55 -0300

    alteração escrita função para troca de classe

--- public/javascript/button_interaction.js
+++ public/javascript/button_interaction.js
@@ -1,12 +1,6 @@
 document.querySelectorAll('.like-button').forEach(card => {
     card.addEventListener('click', event => {
-        if (card.classList.contains('bi-heart')){
-            card.classList.add('bi-heart-fill')
-            card.classList.remove('bi-heart')
-        } else {
-            card.classList.add('bi-heart')
-            card.classList.remove('bi-heart-fill')
-        }
+        card.classList.contains('bi-heart') ? card.classList.replace('bi-heart', 'bi-heart-fill') : card.classList.replace('bi-heart-fill', 'bi-heart')
     })
 })
 
